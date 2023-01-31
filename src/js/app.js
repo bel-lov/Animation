d92355b1997ae9f3a0d5d9092325a2d82b841206
bel-lov <belou.lyuba@yandex.ru>
2023-01-31 21:45:48 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,12 @@
-const btn = document.querySelector('.btn');
-const borderText = document.querySelector('.border-text')
+const btn = document.querySelector(".btn");
+const borderText = document.querySelector(".border-text");
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    if (borderText.classList.contains('show-content')) {
-        borderText.classList.remove('show-content');
-        borderText.style.height = 0;
-        return;
-    }
-    borderText.style.height = '150px';
-    borderText.classList.add('show-content');
-})
-
+  if (borderText.classList.contains("show")) {
+    borderText.classList.remove("show");
+    return;
+  }
+  borderText.classList.add("show");
+});
